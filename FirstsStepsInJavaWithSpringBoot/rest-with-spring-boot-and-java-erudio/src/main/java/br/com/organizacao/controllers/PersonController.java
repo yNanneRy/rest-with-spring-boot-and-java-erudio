37276044a1dd7d987d1abce7ef521b077de8462c
yNanneRy <renangealves@gmail.com>
2023-06-18 01:55:47 -0300

    Adicionado as configurações do banco e comecei a mexer nas entidades

---
+++ FirstsStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/organizacao/controllers/PersonController.java
@@ -0,0 +1,57 @@
+package br.com.organizacao.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.organizacao.model.Person;
+import br.com.organizacao.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+	
+	@RequestMapping(value = "/{id}",
+			method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(
+			@PathVariable(value = "id") String id ) {
+		return service.findById(id);
+	}
+	
+	@RequestMapping(value = "/{id}",
+			method=RequestMethod.DELETE)
+	public void delete(
+			@PathVariable(value = "id") String id ) {
+		service.delete(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person createPerson(@RequestBody Person person) {
+		return service.create(person);
+	}
+	@RequestMapping(method=RequestMethod.PUT,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person updatePerson(@RequestBody Person person) {
+		return service.update(person);
+	}
+}
+ 
